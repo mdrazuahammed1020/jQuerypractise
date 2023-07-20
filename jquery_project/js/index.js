f931a0a26f8406b451cb541df615ec22ab3ff552
mdrazuahammed1020 <135406492+mdrazuahammed1020@users.noreply.github.com>
2023-07-20 21:59:46 +0600

    Add files via upload
    jquery practise assignment

---
+++ jquery_project/js/index.js
@@ -0,0 +1,6 @@
+
+$("#changer").on("click", function() {
+    
+    $(".box1").html('<h2 class="header-txt">i will change create same html tag</h2> <h2 class="header-txt">i will change create same html tag</h2>');
+    
+})
